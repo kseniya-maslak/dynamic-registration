517619817963dc794e8f47d1a437e4c6608a19e6
Kseniya Maslak <kseniya.maslak1@gmail.com>
2022-12-15 09:43:14 +0400

    Create Mock interceptor. Create mock data.

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
 import { RouterModule, Routes } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MockRequestInterceptor } from './shared/mock-request.interceptor';
 
 const routes: Routes = [
   {
@@ -12,6 +15,15 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [RegistrationComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), HttpClientModule],
+  providers: environment.mock
+    ? [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: MockRequestInterceptor,
+          multi: true,
+        },
+      ]
+    : [],
 })
 export class RegistrationModule {}
